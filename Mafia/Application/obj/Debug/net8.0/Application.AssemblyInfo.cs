e69ab8bf0adb7cebd4bcc58bcc164eb1ea75bce4
ilyaghrischenko <ilyahryshchenko@gmail.com>
2024-09-06 21:12:36 +0300

    combined two branches

--- Mafia/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Mafia/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d575f0e4c7ed581d8eb544010bcecf7b35babf31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f455c6fc8f278c387fda3c5bd9240d1e79dba50")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
