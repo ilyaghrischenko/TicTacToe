3529c9dfac9fbc1f21fd46ee5c6d0e5557935277
ilyaghrischenko <ilyahryshchenko@gmail.com>
2024-09-06 21:09:20 +0300

    commit

--- Mafia/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Mafia/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ab8286b437ac633240508297951dd09661f864f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+657df37007c41b43833b4b1b4753f965a1a8776e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
