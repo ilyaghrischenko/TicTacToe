ca8bce18cf6095dec56c655c80033d5fb2cddd21
ilyaghrischenko <ilyahryshchenko@gmail.com>
2024-09-11 15:34:24 +0300

    add validation for registration and showing errors

--- Mafia/Application/wwwroot/scripts/regScript.js
+++ Mafia/Application/wwwroot/scripts/regScript.js
@@ -4,12 +4,12 @@ const form = document.getElementById('regForm');
 
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
-    
+
     const email = document.getElementById('email').value;
     const login = document.getElementById('username').value;
     const password = document.getElementById('password').value;
     const confirmPassword = document.getElementById('confirm-password').value;
-    
+
     const response = await fetch('/api/Account/register', {
         method: 'POST',
         headers: {
@@ -22,10 +22,27 @@ form.addEventListener('submit', async (e) => {
             confirmPassword
         })
     });
-    
+
+    const errorDiv = document.getElementById('errors');
+    errorDiv.innerHTML = '';
+
     if (!response.ok) {
-        console.error(`Error while register: ${response.status}`);
+        const data = await response.json();
+        const errors = data.errors;
+
+        Object.keys(errors).forEach(field => {
+            const errorMessages = errors[field];
+            errorMessages.forEach(message => {
+                const errorElement = document.createElement('div');
+                errorElement.className = 'error';
+                errorElement.innerText = `${field}: ${message}`;
+                errorDiv.appendChild(errorElement);
+            });
+        });
+
+        console.error(`Error while registering: ${response.status}`);
     } else {
-        console.log(`Successful register: ${response.status}`);
+        console.log(`Successful registration: ${response.status}`);
+        window.location.href = '../pages/auth.html';
     }
 });
