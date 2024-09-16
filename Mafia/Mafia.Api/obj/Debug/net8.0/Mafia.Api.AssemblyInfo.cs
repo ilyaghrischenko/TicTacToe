8dd17ce1292c97acbede3bbc292bae206a95e3a2
ilyaghrischenko <ilyahryshchenko@gmail.com>
2024-09-16 15:34:52 +0300

    commit

--- Mafia/Mafia.Api/obj/Debug/net8.0/Mafia.Api.AssemblyInfo.cs
+++ Mafia/Mafia.Api/obj/Debug/net8.0/Mafia.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mafia.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd24a062ffda8bfe9ae5f263ad01da30f53d5f6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+617c403485d921b227a4fe1d679570349731ffec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mafia.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mafia.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
