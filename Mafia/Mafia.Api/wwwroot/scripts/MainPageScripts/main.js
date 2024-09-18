4881252b6fe811d7e7c1cb97fb59bff96f085fcc
ilyaghrischenko <ilyahryshchenko@gmail.com>
2024-09-18 14:29:55 +0300

    add button Manage Friends

--- Mafia/Mafia.Api/wwwroot/scripts/MainPageScripts/main.js
+++ Mafia/Mafia.Api/wwwroot/scripts/MainPageScripts/main.js
@@ -1,8 +1,5 @@
 'use strict';
 
-const avatar = document.getElementById('avatar');
-const login = document.getElementById('login');
-const friendsList = document.getElementById('friendsList');
-const roomsList = document.getElementById('roomsList');
-const statistics = document.getElementById('statistics');
-
+document.getElementById('friends-button').addEventListener('click', function () {
+    window.location.href = '../pages/userFriends.html';
+});
