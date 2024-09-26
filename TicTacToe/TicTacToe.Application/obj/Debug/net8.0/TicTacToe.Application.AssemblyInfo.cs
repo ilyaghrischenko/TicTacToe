37c80534e79fb5005dc16461f03cf2ab05140065
ilyaghrischenko <ilyahryshchenko@gmail.com>
2024-09-26 15:51:34 +0300

    commit

--- TicTacToe/TicTacToe.Application/obj/Debug/net8.0/TicTacToe.Application.AssemblyInfo.cs
+++ TicTacToe/TicTacToe.Application/obj/Debug/net8.0/TicTacToe.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToe.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cab57747fbeff475e1cd6a144d7e80a6c89600ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5ce51b71e86ab042c57b0243ca7f467934570d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToe.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToe.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
