b0a098f5727c278bdcfc3c79fc98e3164aaf7e2c
ilyaghrischenko <ilyahryshchenko@gmail.com>
2024-09-30 20:41:32 +0300

    add scroll bars in userFriends.html and main.html

--- TicTacToe/TicTacToe.Application/obj/Debug/net8.0/TicTacToe.Application.AssemblyInfo.cs
+++ TicTacToe/TicTacToe.Application/obj/Debug/net8.0/TicTacToe.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToe.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45383649c8462f9f2a984a715e9ccaafa9733c21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb14ccbf504933c189ef69074a77c5d78623cfb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToe.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToe.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
