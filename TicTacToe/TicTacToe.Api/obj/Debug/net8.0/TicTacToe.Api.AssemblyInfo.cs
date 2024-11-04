09f807dc122fc57cc95cd22a54b2f7d59ea2b821
ilyaghrischenko <ilyahryshchenko@gmail.com>
2024-11-04 16:59:48 +0200

    commit

--- TicTacToe/TicTacToe.Api/obj/Debug/net8.0/TicTacToe.Api.AssemblyInfo.cs
+++ TicTacToe/TicTacToe.Api/obj/Debug/net8.0/TicTacToe.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToe.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df71ef9eac9ba94721b41bcd4b2507dae16f0dde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0aeb7ddb1eb42fce5ad2ccb1cdf99748d8a51ab7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToe.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToe.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
