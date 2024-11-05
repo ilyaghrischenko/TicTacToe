53e724765d0848aded59751fd6846bcc8ffd2aec
ilyaghrischenko <ilyahryshchenko@gmail.com>
2024-11-05 15:28:35 +0200

    push

--- TicTacToe/TicTacToe.DTO/obj/Debug/net8.0/TicTacToe.DTO.AssemblyInfo.cs
+++ TicTacToe/TicTacToe.DTO/obj/Debug/net8.0/TicTacToe.DTO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToe.DTO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bca3feb5ee85596eb9cdd3dd6baaa4ff5136dce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae42e8d9141277b9aed557f33953bf3ea65f952c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToe.DTO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToe.DTO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
