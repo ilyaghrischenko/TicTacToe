d4884458856b62b0781afa2878ed53f5b1e90d5c
ilyaghrischenko <ilyahryshchenko@gmail.com>
2024-11-06 20:35:30 +0200

    changed settings validators, added only latin letters

--- TicTacToe/TicTacToe.Validation/SettingsModelsValidators/ChangeLoginModelValidator.cs
+++ TicTacToe/TicTacToe.Validation/SettingsModelsValidators/ChangeLoginModelValidator.cs
@@ -9,6 +9,7 @@ public class ChangeLoginModelValidator : AbstractValidator<ChangeLoginModel>
     {
         RuleFor(x => x.LoginInput)
             .NotEmpty().WithMessage("This field is required")
-            .Length(4, 20).WithMessage("Login must be between 4 and 20 characters long");
+            .Length(4, 20).WithMessage("Login must be between 4 and 20 characters long")
+            .Matches("^[a-zA-Z0-9]+$").WithMessage("Login must contain only Latin letters and numbers");
     }
 }
