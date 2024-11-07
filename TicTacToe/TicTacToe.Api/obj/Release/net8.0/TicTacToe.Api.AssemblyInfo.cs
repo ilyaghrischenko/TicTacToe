ea30a7791b262eec8e9e727160912d9aa0894a6a
ilyaghrischenko <ilyahryshchenko@gmail.com>
2024-11-07 13:09:58 +0200

    done

--- TicTacToe/TicTacToe.Api/obj/Release/net8.0/TicTacToe.Api.AssemblyInfo.cs
+++ TicTacToe/TicTacToe.Api/obj/Release/net8.0/TicTacToe.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToe.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5efc73f185b8df9871a76ea8d2c1bb9baa24f2ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+903ffafcdc363eaa8748aa3ae6186a7b06a45db1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToe.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToe.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
