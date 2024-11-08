2ca0612aa172e077904424af9fa23b376055524b
ilyaghrischenko <ilyahryshchenko@gmail.com>
2024-11-08 16:38:02 +0200

    testing

--- TicTacToe/TicTacToe.DTO/obj/Release/net8.0/TicTacToe.DTO.AssemblyInfo.cs
+++ TicTacToe/TicTacToe.DTO/obj/Release/net8.0/TicTacToe.DTO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToe.DTO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e26316c3f0fa9f48ba6b67d35c5cadc12812ffd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e0215dbdb9a190f5377ed70c8ca11022db564d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToe.DTO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToe.DTO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TicTacToe/TicTacToe.Validation/obj/Release/net8.0/TicTacToe.Validation.AssemblyInfo.cs
+++ TicTacToe/TicTacToe.Validation/obj/Release/net8.0/TicTacToe.Validation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToe.Validation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e26316c3f0fa9f48ba6b67d35c5cadc12812ffd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e0215dbdb9a190f5377ed70c8ca11022db564d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToe.Validation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToe.Validation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
