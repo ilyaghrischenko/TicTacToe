d8da719a770fa72ee46846adeb227926a239114f
ilyaghrischenko <ilyahryshchenko@gmail.com>
2024-11-10 21:19:25 +0200

    merge

--- TicTacToe/TicTacToe.Contracts/obj/Debug/net8.0/TicTacToe.Contracts.AssemblyInfo.cs
+++ TicTacToe/TicTacToe.Contracts/obj/Debug/net8.0/TicTacToe.Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToe.Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62013414123b52bb56cebb4a4e5052215aafb86b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e108a2abd7834cbc7e84c4ff0f8435abf611ac3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToe.Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToe.Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
