c36b30192202ef05882751837ed6b0277e882451
ilyaghrischenko <ilyahryshchenko@gmail.com>
2024-11-12 16:35:23 +0200

    version 1.4

--- TicTacToe/TicTacToe.Data/obj/Release/net8.0/TicTacToe.Data.AssemblyInfo.cs
+++ TicTacToe/TicTacToe.Data/obj/Release/net8.0/TicTacToe.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToe.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+576aadce6471d203433097524f28dbff5e3425a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4be5e4ce356976a3e1f3312fbd1d5a08b3364094")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToe.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToe.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
