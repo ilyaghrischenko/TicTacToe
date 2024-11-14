3f03ee3aeda59cb174e9f93adf40bef768b1a43d
ilyaghrischenko <ilyahryshchenko@gmail.com>
2024-11-14 15:38:15 +0200

    add new project for viewing bugs

---
+++ BugView/BugView/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace BugView;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
---
+++ BugView/BugView/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Windows;
+using TicTacToe.Domain.DbModels;
+
+namespace BugView;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        LoadBugsAsync();
+    }
+
+    private async Task LoadBugsAsync()
+    {
+        using HttpClient client = new HttpClient();
+        string apiUrl = "http://192.168.1.77/api/Bug/getBugs";
+            
+        try
+        {
+            var bugs = await client.GetFromJsonAsync<List<Bug>>(apiUrl);
+            BugDataGrid.ItemsSource = bugs;
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Error loading bugs: {ex.Message}");
+        }
+    }
+}
